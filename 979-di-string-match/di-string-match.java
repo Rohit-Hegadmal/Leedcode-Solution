601218ea2eca078f75c36d10ea3cf083746f647b
Rohit Hegdamal <rohithegadmal77@gmail.com>
2026-09-07 16:07:54 +0530

    Time: 2 ms (99.30%) | Memory: 47.4 MB (26.05%) - LeetSync

---
+++ 979-di-string-match/di-string-match.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] diStringMatch(String s) {
+        int n = s.length();
+        int ans[] = new int[n+1];
+        int left = 0; 
+        int right = n;
+        for(int i=0;i<n;++i)
+        {
+            if(s.charAt(i)=='I')
+            {
+                ans[i]=left++;
+            }
+            else{
+                ans[i]=right--;
+            }
+        }
+        ans[n] = left;
+        return ans;
+    }
+}
